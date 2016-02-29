e0a54247749ab3a9ff4725a9d5a90a3d29973671
Russ Wells <russ@russdigital.com>
2016-02-29 03:38:03 -0800

    docked the bottom bar

--- public/assets/js/header.js
+++ public/assets/js/header.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
-
+		
+		
 		$(".metalBar").addClass('fixedLowerBar');
 	    $(window).scroll(function() {
+	    	var footer = $('footer');
+	    	var footerPosition = footer.offset().top;
+			//var footerPosition = document.getElementById('footer').offsetHeight;
 	        var scroll = $(window).scrollTop();
+	        var bottomEdge = scroll + window.innerHeight;
 	        var scrollBottom = $(document).height()-$("footer").height();
 	        if (scroll >= 120) {
 	        	$("#navMark").addClass('fixedNav');
@@ -11,9 +16,12 @@ $(document).ready(function () {
 	        	$("#navMark").removeClass('fixedNav');
 	        	$("#fixedHeaderExpander").removeClass('expanded');
 	        }
-	        if(scroll >= scrollBottom){
-	        	alert("scrollBottom");
-	        	$(".metalBar").removeClass('fixedLowerBar');
+	        if(bottomEdge >= footerPosition+60){
+	        	$(".metalBar").addClass('metalBarUnstuck');
+	        }else{
+	        	$(".metalBar").removeClass('metalBarUnstuck');
 	        }
+	        console.log("Scroll: "+ scroll+", Bottom:, "+ bottomEdge + ", footerStart: "+ footerPosition);
+
 	    });
 });
